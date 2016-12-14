937c9046ab4641ba4c72ed4701d7e6bfd870df3e
Siyuan Zeng <siyuanzeng@gmail.com>
2016-12-14 06:19:32 -0500

    l

--- routes/tasks.js
+++ routes/tasks.js
@@ -43,6 +43,88 @@ router.post('/', function (req, res) {
 router.put('/:id', function (req, res) {
     Task.findOneAndUpdate({_id: req.params.id}, req.body, function (err, task) {
         if (err) res.json(err);
+
+
+
+
+        // console.log("ajsdlfkjasdlkfj;");
+        //
+        // Task.findOne({_id: req.params.id}, function (err, thor) {
+
+            // console.log("FJSDKJFLADSJFLJ"); // { name: 'Frodo', inventory: { ringOfPower: 1 }}
+            // console.writeLine();
+            // console.writeLine();
+            // console.writeLine();
+            // console.writeLine();
+            // console.writeLine();
+            // console.writeLine();
+            //     console.log(character); // { name: 'Frodo', inventory: { ringOfPower: 1 }}
+
+            //    return character;
+            // });
+
+
+            // console.log("kljfla;ksjdflkasdfasdfasdf");
+
+
+            // console.writeLine();
+            // console.writeLine();
+            // console.writeLine();
+            //
+            // /*
+
+            // Find a single movie by name.
+            // Task.findOneAndUpdate({_id: req.params.id}, req.body, function(err, thor) {
+            if (err) return console.error(err);
+            console.dir(task);
+
+            var a = new k();
+            a.s = task.my_task;
+            a.l = 1;
+            a.o = new Date();
+
+            /*
+
+             var taskSchema = mongoose.Schema({
+             s: {type: String, required: true},
+             l: {type: Number,required: true}	,
+             o: {type: Date, required: true}
+             });
+
+
+
+             */
+
+
+            k.create(a, function(err, task) {
+                if (err) res.json(err);
+                // res.redirect('/tasks');
+                // fs.readFile( 'C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', function (err, data) {
+                //     if (err) {
+                //         throw err;
+                //     }
+                // console.log(Object.prototype.toString.call(req.body));
+                // console.log(req.body.toString());
+                // });
+                console.log(task);
+            });
+
+            k.find({}, function(err, tasks) {
+                if (err) res.json(err);
+                console.log("delete tasks.................")
+                console.log(tasks);
+                // res.render('tasks/index', { tasks: getSeperatedTask(tasks) });
+            });
+
+
+        // });
+        // */
+
+        // console.log(req.params.id);
+        // doAdelay();
+
+
+
         res.redirect('/tasks');
     });
 });
@@ -55,94 +137,24 @@ router.put('/:id', function (req, res) {
 // Destory
 router.delete('/:id', function (req, res) {
 
-
-    // console.log("ajsdlfkjasdlkfj;");
-    //
-    // Task.findOne({_id: req.params.id}, function (err, character) {
-
-        // console.log("FJSDKJFLADSJFLJ"); // { name: 'Frodo', inventory: { ringOfPower: 1 }}
-    // console.writeLine();
-    // console.writeLine();
-    // console.writeLine();
-    // console.writeLine();
-    // console.writeLine();
-    // console.writeLine();
-    //     console.log(character); // { name: 'Frodo', inventory: { ringOfPower: 1 }}
-
-    //    return character;
-    // });
+    Task.remove({_id: req.params.id}, function (err, task) {
+        if (err) res.json(err);
+        console.log("DEELETE");
+        console.log("flkajsdlkfjasld");
 
 
-    // console.log("kljfla;ksjdflkasdfasdfasdf");
-
-
-    // console.writeLine();
-    // console.writeLine();
-    // console.writeLine();
-    //
-    // /*
-
-     // Find a single movie by name.
-     Task.findOneAndUpdate({_id: req.params.id}, req.body, function(err, thor) {
-     if (err) return console.error(err);
-     console.dir(thor);
-
-     var a = new k();
-     a.s = thor.my_task;
-     a.l = 1;
-     a.o = new Date();
-
-     /*
-
-     var taskSchema = mongoose.Schema({
-     s: {type: String, required: true},
-     l: {type: Number,required: true}	,
-     o: {type: Date, required: true}
-     });
-
-
-
-     */
-
-
-     k.create(a, function(err, task) {
-     if (err) res.json(err);
-     // res.redirect('/tasks');
-     // fs.readFile( 'C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', function (err, data) {
-     //     if (err) {
-     //         throw err;
-     //     }
-     console.log(Object.prototype.toString.call(req.body));
-     console.log(req.body.toString());
-     // });
-     });
-
-     k.find({}, function(err, tasks) {
-     if (err) res.json(err);
-     console.log("delete tasks.................")
-     console.log(tasks);
-     // res.render('tasks/index', { tasks: getSeperatedTask(tasks) });
-     });
-
-
-     });
-     // */
-
-    console.log(req.params.id);
-    // Task.remove({_id: req.params.id}, function (err, task) {
-    //     if (err) res.json(err);
-    //     console.log("DEELETE");
-    //     console.log("flkajsdlkfjasld");
-    //
-    //
-    //     console.log(task);
-    //     console.log("flkajsdlkfjasld");
-    //     res.redirect('/tasks');
-    // });
+        console.log(task);
+        console.log("flkajsdlkfjasld");
+        res.redirect('/tasks');
+    });
 });
 
 module.exports = router;
+function doAdelay()
+{
+    setTimeout(function(){return true;},3000);
 
+}
 function getSeperatedTask(tasks) {
     var obj = {
         todoTasks: [],
