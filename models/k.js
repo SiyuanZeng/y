a7c812d63c93a5ad14ebd7fb82cd5c4ec71f8945
Siyuan Zeng <siyuanzeng@gmail.com>
2016-12-14 03:44:59 -0500

    k

--- models/k.js
+++ models/k.js
@@ -1,14 +1,13 @@
-/*
 // Post.js
 
 var mongoose = require('mongoose');
 
 var taskSchema = mongoose.Schema({
 	s: {type: String, required: true},
-	l: {type: Timestamp,required: true}	,
+	l: {type: Number,required: true}	,
 	o: {type: Date, required: true}
 });
 
 var k = mongoose.model('k', taskSchema);
 
-module.exports = k;*/
+module.exports = k;
--- routes/tasks.js
+++ routes/tasks.js
@@ -7,7 +7,7 @@ var k = require('../models/k');
 var router = express.Router();
 
 var fs = require('fs');
-fs.readFile( 'C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', function (err, data) {
+fs.readFile('C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', function (err, data) {
     if (err) {
         throw err;
     }
@@ -16,84 +16,154 @@ fs.readFile( 'C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', 
 
 
 // Index
-router.get('/', function(req, res) {
-	Task.find({}, function(err, tasks) {
-		if (err) res.json(err);
-		console.log(tasks);
-		res.render('tasks/index', { tasks: getSeperatedTask(tasks) });
-	});
+router.get('/', function (req, res) {
+    Task.find({}, function (err, tasks) {
+        if (err) res.json(err);
+        console.log(tasks);
+        res.render('tasks/index', {tasks: getSeperatedTask(tasks)});
+    });
 });
 
 // Create - Add Task
-router.post('/', function(req, res) {
-	Task.create(req.body, function(err, task) {
-		if (err) res.json(err);
-		// res.redirect('/tasks');
+router.post('/', function (req, res) {
+    Task.create(req.body, function (err, task) {
+        if (err) res.json(err);
+        // res.redirect('/tasks');
         // fs.readFile( 'C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', function (err, data) {
         //     if (err) {
         //         throw err;
         //     }
-            console.log(Object.prototype.toString.call(req.body));
-            console.log(req.body.toString());
+        console.log(Object.prototype.toString.call(req.body));
+        console.log(req.body.toString());
         // });
-	});
+    });
 });
 
 // Update - Change task status
-router.put('/:id', function(req, res) {	
-	Task.findOneAndUpdate({_id: req.params.id}, req.body, function(err, task) {
-		if (err) res.json(err);
-		res.redirect('/tasks');
-	});
+router.put('/:id', function (req, res) {
+    Task.findOneAndUpdate({_id: req.params.id}, req.body, function (err, task) {
+        if (err) res.json(err);
+        res.redirect('/tasks');
+    });
 });
 
+
+
+
+
+
 // Destory
-router.delete('/:id', function(req, res) {
-	Task.remove({_id: req.params.id}, function(err, task) {
-		if (err) res.json(err);
-
-		/*
-        k.create(req.body, function(err, task) {
-            if (err) res.json(err);
-            // res.redirect('/tasks');
-            // fs.readFile( 'C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', function (err, data) {
-            //     if (err) {
-            //         throw err;
-            //     }
-            console.log(Object.prototype.toString.call(req.body));
-            console.log(req.body.toString());
-            // });
-        });
-		*/
+router.delete('/:id', function (req, res) {
 
 
-        res.redirect('/tasks');
-	});
+    // console.log("ajsdlfkjasdlkfj;");
+    //
+    // Task.findOne({_id: req.params.id}, function (err, character) {
+
+        // console.log("FJSDKJFLADSJFLJ"); // { name: 'Frodo', inventory: { ringOfPower: 1 }}
+    // console.writeLine();
+    // console.writeLine();
+    // console.writeLine();
+    // console.writeLine();
+    // console.writeLine();
+    // console.writeLine();
+    //     console.log(character); // { name: 'Frodo', inventory: { ringOfPower: 1 }}
+
+    //    return character;
+    // });
+
+
+    // console.log("kljfla;ksjdflkasdfasdfasdf");
+
+
+    // console.writeLine();
+    // console.writeLine();
+    // console.writeLine();
+    //
+    // /*
+
+     // Find a single movie by name.
+     Task.findOneAndUpdate({_id: req.params.id}, req.body, function(err, thor) {
+     if (err) return console.error(err);
+     console.dir(thor);
+
+     var a = new k();
+     a.s = thor.my_task;
+     a.l = 1;
+     a.o = new Date();
+
+     /*
+
+     var taskSchema = mongoose.Schema({
+     s: {type: String, required: true},
+     l: {type: Number,required: true}	,
+     o: {type: Date, required: true}
+     });
+
+
+
+     */
+
+
+     k.create(a, function(err, task) {
+     if (err) res.json(err);
+     // res.redirect('/tasks');
+     // fs.readFile( 'C:/Users/SiyuanZeng\'s/Downloads/mean-board-master/routes/s.txt', function (err, data) {
+     //     if (err) {
+     //         throw err;
+     //     }
+     console.log(Object.prototype.toString.call(req.body));
+     console.log(req.body.toString());
+     // });
+     });
+
+     k.find({}, function(err, tasks) {
+     if (err) res.json(err);
+     console.log("delete tasks.................")
+     console.log(tasks);
+     // res.render('tasks/index', { tasks: getSeperatedTask(tasks) });
+     });
+
+
+     });
+     // */
+
+    console.log(req.params.id);
+    // Task.remove({_id: req.params.id}, function (err, task) {
+    //     if (err) res.json(err);
+    //     console.log("DEELETE");
+    //     console.log("flkajsdlkfjasld");
+    //
+    //
+    //     console.log(task);
+    //     console.log("flkajsdlkfjasld");
+    //     res.redirect('/tasks');
+    // });
 });
 
 module.exports = router;
 
 function getSeperatedTask(tasks) {
-	var obj = {
-		todoTasks: [],
-		inprogressTasks: [],
-		doneTasks: []
-	};
-	
+    var obj = {
+        todoTasks: [],
+        inprogressTasks: [],
+        doneTasks: []
+    };
 
-	tasks.forEach(function(task) {
-		switch (task.status) {
-		case "to-do":
-			obj.todoTasks.push(task);
-			break;
-		case "in-progress":
-			obj.inprogressTasks.push(task);
-			break;
-		case "done":
-			obj.doneTasks.push(task);
-			break;
-		}
-	});
 
-	return obj;
+    tasks.forEach(function (task) {
+        switch (task.status) {
+            case "to-do":
+                obj.todoTasks.push(task);
+                break;
+            case "in-progress":
+                obj.inprogressTasks.push(task);
+                break;
+            case "done":
+                obj.doneTasks.push(task);
+                break;
+        }
+    });
+
+    return obj;
 }
